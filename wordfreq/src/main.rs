8df61a6bef54ae7349faca775f22e7b1923d219c
GanZ2016 <ganzhang@u.northwestern.edu>
2017-04-13 10:52:11 -0500

    add test case

--- wordfreq/src/main.rs
+++ wordfreq/src/main.rs
@@ -60,7 +60,7 @@ ASSUMPTIONS
 use std::io::{BufRead,BufReader,Read,stdin,Write,stdout};
 use std::collections::HashMap;
 fn main() {
-    word_count(&read_input(stdin()));
+    write_output(stdout(),word_sort(&word_count(&read_input(stdin()))));
 }
 //standard input and store the result in a string
 pub fn read_input<R: Read>(reader: R) -> String {
@@ -94,8 +94,8 @@ mod read_input_test {
 }
 }
 
-pub fn word_count(input: &str)  {
-    let mut map: HashMap<String, u32> = HashMap::new();
+pub fn word_count(input: &str) -> HashMap<String, usize> {
+    let mut map: HashMap<String, usize> = HashMap::new();
     let lower = input.to_lowercase(); //turn the input string into lowercase
     let slice: &str = lower.as_ref();
     //split the input string and search the word in hashmap, get value and plus 1
@@ -103,14 +103,53 @@ pub fn word_count(input: &str)  {
         *map.entry(word.to_string()).or_insert(0)+=1;
     }
 
-    //sort the hashmap and put the result in a vector
-    let mut count_vec: Vec<(&String, &u32)> = map.iter().collect();
-    count_vec.sort_by(|a, b| b.1.cmp(a.1));    
-    //print elements in the vector line by line
-    write_output(stdout(),count_vec);
+    map
 }
 
-pub fn write_output<W: Write>(mut writer: W, r:Vec<(&String,&u32)>) {
+#[cfg(test)]
+mod test_word_count{
+    use super::word_count;
+    use std::collections::HashMap;
+    #[test]
+    fn count_some_words() {
+        let input = "Hello world world! ";
+        let map: HashMap<String, usize>  = word_count(input);
+        assert_eq!(map[&"hello".to_string()], 1);
+        assert_eq!(map[&"world".to_string()], 2);
+    }
+}
+
+fn word_sort(map: &HashMap<String, usize>) -> Vec<(&String, &usize)> {
+    //sort the hashmap and put the result in a vector
+    let mut count_vec: Vec<(&String, &usize)> = map.iter().collect();
+    count_vec.sort_by(|a, b| b.1.cmp(a.1));
+    //print elements in the vector line by line
+    count_vec
+}
+
+#[cfg(test)]
+mod test_word_sort{
+    use super::word_sort;
+    use std::collections::HashMap;
+    #[test]
+    fn word_sort_test() {
+        let mut map: HashMap<String, usize> = HashMap::new();
+        map.insert("Hello".to_string(),2);
+        map.insert("world".to_string(),3);
+        map.insert("test".to_string(),4);
+        let count = word_sort(&map);
+        assert_eq!(count[0].0,"test");
+        assert_eq!(count[0].1, &(4 as usize));
+        assert_eq!(count[1].0,"world");
+        assert_eq!(count[1].1, &(3 as usize));
+        assert_eq!(count[2].0,"Hello");
+        assert_eq!(count[2].1, &(2 as usize));
+    }  
+}
+
+
+
+pub fn write_output<W: Write>(mut writer: W, r:Vec<(&String,&usize)>) {
   if r.is_empty() {
       write!(writer, "No word found.\n").unwrap();
   } 
@@ -119,5 +158,28 @@ pub fn write_output<W: Write>(mut writer: W, r:Vec<(&String,&u32)>) {
         let (key,value) = r[i].clone();
         write!(writer,"{}: {}\n", key,value).unwrap();   
         }
+    }
+}
+
+#[cfg(test)]
+mod test_write_output{
+	use super::write_output;
+	use std::io::Cursor;
+    use super::word_sort;
+    use std::collections::HashMap;
+	#[test]
+	fn output_result(){
+        let mut map: HashMap<String, usize> = HashMap::new();
+        map.insert("hello".to_string(),1);
+        map.insert("world".to_string(),3);
+        map.insert("test".to_string(),4);
+        let count = word_sort(&map);
+		assert_write("test: 4\nworld: 3\nhello: 1\n", count);
+		}
+
+    fn assert_write(expected: &str, result: Vec<(&String,&usize)>) {
+        let mut writer = Cursor::new(vec![]);
+        write_output(&mut writer, result);
+        assert_eq!(expected.as_bytes(), &*writer.into_inner());
     }
 }
